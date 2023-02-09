79fa4efd1c9ecceeb4463b3fa4be1d1e3dd9c360
EggsyJ <58285674+JeonChanMin@users.noreply.github.com>
2023-02-10 05:17:33 +0900

    Practice-051

---
+++ C_Nth/03_Intermediate/051.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+#define ENTER 13
+
+int main(void) {
+	
+	int ch;
+	
+	printf("아스키 코드로 변환할 키를 누르세요...\n");
+	printf("ENTER 키를 누르면 프로그램은 종료됩니다.\n");
+	printf("\n");
+	
+	do {
+		ch = getch();
+		
+		if(ch != ENTER) {
+			printf("Char: %c \t ASCII Code: %d\n", ch, ch);
+		}
+	}
+	while(ch != ENTER);
+	
+	return 0;
+}
