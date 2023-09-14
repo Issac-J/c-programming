1d10a1cb10212a24d190cca0bac648a1f222ed8a
Issac-J <fordev0905@gmail.com>
2023-09-15 00:22:33 +0900

    practice 011

---
+++ c-nth/02_Beginner/011.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(int argc, char* argv[]) {
+	
+	char ch;
+	unsigned char j;
+	char k;
+	
+	ch = 200;
+	j = 200;
+	k = 'a';
+	
+	printf("문자형 변수 ch : %d\n", ch);
+	printf("문자형 변수  j : %d\n", j);
+	printf("문자형 변수  k : %d\n", k);
+	
+	return 0;
+}
