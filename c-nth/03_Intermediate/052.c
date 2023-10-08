9883f1fae8024a08e23fcb11a169fffa423df60e
EggsyJ <fordev0905@gmail.com>
2023-10-09 06:40:48 +0900

    practice 052

--- c-nth/03_Intermediate/052.c
+++ c-nth/03_Intermediate/052.c
@@ -1,21 +1,30 @@
-//	문자 입력받기: putch()  
-
+//	문자 입력받기: putch()
 #include <stdio.h>
 
+// print 함수를 선언 
 int print(char *string);
 
 int main(void) {
-	
 	print("This is a putch function!!!");
 	
 	return 0;
 }
 
 int print(char *string) {
-	
 	int strLength = 0;
 	
+	/*
+		문자열은 NULL로 종결되기 때문에 
+		현재 string이 가리키는 번지의 값이 NULL 인지를 확인합니다.
+		string이 가리는 번지의 값이 NULL이 될 때까지 while 문을 반복합니다.  
+	*/
 	while( *string != (char)NULL) {
+		
+		/*
+			현재 string이 가리키는 번지의 문자를 출력합니다. 
+			string: 메모리주소(번지)를 저장하는 변수 
+			*string: 메모리주소가 가리키는 값 
+		*/
 		putch(*string);
 		string++;
 		strLength++;
