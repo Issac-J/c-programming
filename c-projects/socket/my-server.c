956fe3b2be3996c2f5402d2fcd92e410aaa759d2
EggsyJ <fordev0905@gmail.com>
2024-01-17 14:28:55 +0900

    modifiy server.c

--- c-projects/socket/my-server.c
+++ c-projects/socket/my-server.c
@@ -4,17 +4,12 @@
 
 int main(int argc, char *argv[]) {
 
-	int server_socket;
+	int server_socket, client_socket;
 	
-	struct sockaddr_in server_addr;
+	struct sockaddr_in server_addr client_addr;
 
 	// 소켓 생성
 	server_socket = socket(PF_INET, SOCK_STREAM, 0);
-
-	// 소켓 설정: 전송 방식, 주소, 포트
-	server_addr.sin_family = AF_INET;
-	server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-	serer_addr.sin_port = htons(7989);	
 
 	return 0;
 }
